2d7c79a6121c41eaf16027d43564c0de5702b60b
Priyanka Budre <priyankabudre.01@gmail.com>
2020-08-09 00:06:23 +0530

    Remove.bat

--- Remove.bat
+++ Remove.bat
@@ -1,11 +1,16 @@
 @echo off
-Set runningContainer = $(docker ps -aqf name=expresscontainer)
-Set exitedContainer = $(docker ps -aq -f status=exited -f name=expresscontainer)
-
-if not  [%runningContainer%] == [] (
+set runningContainer = ""
+set exitedContainer = ""
+FOR /F "tokens=1 delims=" %%I in ('docker ps -aq -f "name=expresscontainer"') do set runningContainer=%%I
+FOR /F "tokens=1 delims=" %%A in ('docker ps -aq -f "status=exited" -f "name=expresscontainer"') do set exitedContainer=%%A
+if not [%runningContainer%] == [] (
 		docker stop expresscontainer
     docker rm expresscontainer
+    echo "Removed"
 
 )else if not [%exitedContainer%] == [] (
     docker rm expresscontainer
+    echo "exited"
 )
+set "runningContainer="
+set "exitedContainer="
