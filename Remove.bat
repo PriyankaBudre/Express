099aa1d5c02bf80a1e422011937b3675e0902d34
Priyanka Budre <priyankabudre.01@gmail.com>
2020-08-08 20:48:42 +0530

    Remove.bat

--- Remove.bat
+++ Remove.bat
@@ -1,7 +1,8 @@
 if not "$(docker ps -aqf name=expresscontainer)"=="" (
 		docker stop expresscontainer
     docker rm expresscontainer
-)
-else if not "$(docker ps -aq -f status=exited -f name=expresscontainer)" == ""
+)else(
+ if not "$(docker ps -aq -f status=exited -f name=expresscontainer)" == ""
     docker rm expresscontainer
+)
 )
